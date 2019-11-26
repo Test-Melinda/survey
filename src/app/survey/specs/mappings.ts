78056bd31026045fc3ac438ff068d2025a3842fe
Claudio Venturini <claudio.venturini1@tiscali.it>
2019-11-26 21:50:43 +0100

    Fix missing mappings

--- src/app/survey/specs/mappings.ts
+++ src/app/survey/specs/mappings.ts
@@ -3,10 +3,10 @@ import * as mappingDefault from "./mapping-default";
 
 import * as mappingIT from "./mapping-it";
 import * as mappingCH from "./mapping-ch";
-//import * as mappingSI from "./mapping-si";
-//import * as mappingAT from "./mapping-at";
-//import * as mappingFR from "./mapping-fr";
-//import * as mappingDE from "./mapping-de";
+import * as mappingSI from "./mapping-si";
+import * as mappingAT from "./mapping-at";
+import * as mappingFR from "./mapping-fr";
+import * as mappingDE from "./mapping-de";
 
 export const mappings = new Map<String, Map<string, {
     gid: number,
@@ -16,9 +16,9 @@ export const mappings = new Map<String, Map<string, {
 
 mappings.set('default', mappingDefault.mapping);
 
-//mappings.set('at', mappingAT.mapping);
-//mappings.set('de', mappingDE.mapping);
-//mappings.set('fr', mappingFR.mapping);
+mappings.set('at', mappingAT.mapping);
+mappings.set('de', mappingDE.mapping);
+mappings.set('fr', mappingFR.mapping);
 mappings.set('it', mappingIT.mapping);
-//mappings.set('si', mappingSI.mapping);
+mappings.set('si', mappingSI.mapping);
 mappings.set('ch', mappingCH.mapping);
