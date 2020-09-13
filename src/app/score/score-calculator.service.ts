eb1778a6d83ca3610ea84af813e372f8ec0cf3da
Claudio Venturini <claudio.venturini1@tiscali.it>
2020-09-13 22:13:10 +0200

    Update score calculation

--- src/app/score/score-calculator.service.ts
+++ src/app/score/score-calculator.service.ts
@@ -13,9 +13,7 @@ export class ScoreCalculatorService {
 			!!responseData.Q3 &&
 			(!!responseData.Q7dot1IT || !!responseData.Q7dot1AU || !!responseData.Q7dot1SL || !!responseData.Q7dot1SW || !!responseData.Q7dot1FR || !!responseData.Q7dot1GE) &&
 			!!responseData.Q7dot2 &&
-			!!responseData.Q7dot3 &&
-			!!responseData.Q8 &&
-			!!responseData.Q10
+			!!responseData.Q7dot3
 		);
 	}
 
@@ -25,9 +23,7 @@ export class ScoreCalculatorService {
 			Q3: 0,
 			Q7_1: 0,
 			Q7_2: 0,
-			Q7_3: 0,
-			Q8: 0,
-			Q10: 0
+			Q7_3: 0
 		}
 
 		// Question Q2
@@ -75,30 +71,13 @@ export class ScoreCalculatorService {
 
 		// Question Q7.3
 		q = responseData.Q7dot3;
-		if (q == "A2") {
+		if (q == "2") {
 			scores.Q7_3 = 1;
-		}
-
-		// Question Q8
-		q = responseData.Q8;
-		if (q) {
-			if ("1" in q || "9" in q) {
-				scores.Q8 = 0.6;
-			}
-			else if ("2" in q || "3" in q || "4" in q || "5" in q || "6" in q || "7" in q || "8" in q) {
-				scores.Q8 = 0.4;
-			}
-		}
-
-		// Question Q10
-		q = responseData.Q10;
-		if (q && "8" in q) {
-			scores.Q10 = -1;
 		}
 
 		console.log("Answers scores", scores);
 
-		return scores.Q2 + (scores.Q3 * 1.3) + (scores.Q7_1 * 0.5) + (scores.Q7_2 * 0.5) + (scores.Q7_3 * 0.5) + scores.Q8 + scores.Q10;
+		return scores.Q2 + (scores.Q3 * 1.3) + (scores.Q7_1 * 0.5) + (scores.Q7_2 * 0.5) + (scores.Q7_3 * 0.5);
 	}
 
 }
